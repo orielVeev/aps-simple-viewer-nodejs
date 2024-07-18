5664e9e9c0f3dc1ba10becb70cb953cd27ef71b2
orielVeev <oriel@veev.com>
2024-07-18 16:40:47 +0300

    add viewes

--- wwwroot/CustomTreeViewPanel.js
+++ wwwroot/CustomTreeViewPanel.js
@@ -1,102 +1,107 @@
 const MY_FOLDER_DATA = {
-    path: '/home',
-    name: 'Home',
-    entries: [
+  path: "/home",
+  name: "Home",
+  entries: [
+    {
+      path: "/home/docs",
+      name: "Documents",
+      entries: [
         {
-            path: '/home/docs',
-            name: 'Documents',
-            entries: [
-                {
-                    path: '/home/docs/drawing1.pdf',
-                    name: 'drawing1.pdf'
-                }
-            ]
+          path: "dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6dGo4Z2hxY2JscHpjYnVycjcxMmd5ZmhuYTYzdXVib2wtYmFzaWMtYXBwL1AxMDA5LVNGSDJFLUExX29yaWVsTjRBVlgucnZ0",
+          name: "FirstModel.rvt",
         },
         {
-            path: '/home/images',
-            name: 'Images',
-            entries: [
-                {
-                    path: '/home/images/image1.png',
-                    name: 'image1.png'
-                },
-                {
-                    path: '/home/images/old',
-                    name: 'Old',
-                    entries: [
-                        {
-                            path: '/home/images/old/image1.png',
-                            name: 'image1.png'
-                        },
-                        {
-                            path: '/home/images/old/image2.png',
-                            name: 'image2.png'
-                        }                        
-                    ]
-                }
-            ]
-        }
-    ]
+          path: "dXJuOmFkc2sud2lwcHJvZDpmcy5maWxlOnZmLlRlOS1CTVV6UU8yYjZJZEZ4TDk2a3c_dmVyc2lvbj0y",
+          name: "SecondModel.rvt",
+        },
+      ],
+    },
+  ],
 };
 
 class CustomTreeDelegate extends Autodesk.Viewing.UI.TreeDelegate {
-    isTreeNodeGroup(node) {
-        return node.entries && node.entries.length > 0;
-    }
+  isTreeNodeGroup(node) {
+    return node.entries && node.entries.length > 0;
+  }
 
-    getTreeNodeId(node) {
-        return node.path;
-    }
+  getTreeNodeId(node) {
+    return node.path;
+  }
 
-    getTreeNodeLabel(node) {
-        return node.name;
-    }
+  getTreeNodeLabel(node) {
+    return node.name;
+  }
 
-    getTreeNodeClass(node) {
-        node.children && node.children.length > 0 ? 'group' : 'leaf';
-    }
+  getTreeNodeClass(node) {
+    node.children && node.children.length > 0 ? "group" : "leaf";
+  }
 
-    forEachChild(node, callback) {
-        for (const child of node?.entries) {
-            callback(child);
-        }
+  forEachChild(node, callback) {
+    for (const child of node?.entries) {
+      callback(child);
     }
+  }
 
-    onTreeNodeClick(tree, node, event) {
-        console.log('click', tree, node, event);
-    }
+  onTreeNodeClick(tree, node, event) {
+    console.log("click", tree, node, event);
+    const { path } = node;
 
-    onTreeNodeDoubleClick(tree, node, event) {
-        console.log('double-click', tree, node, event);
-    }
+    let urn = path;
+    Autodesk.Viewing.Document.load(
+      "urn:" + urn,
+      (doc) => {
+        console.log(doc.getRoot().getDefaultGeometry());
+        return NOP_VIEWER.loadDocumentNode(
+          doc,
+          doc.getRoot().getDefaultGeometry()
+        );
+      },
+      (code, message, errors) => {
+        console.error(code, message, errors);
+        alert("Could not load model. See console for more details.");
+      }
+    );
+  }
 
-    onTreeNodeRightClick(tree, node, event) {
-        console.log('right-click', tree, node, event);
-    }
+  onTreeNodeDoubleClick(tree, node, event) {
+    console.log("double-click", tree, node, event);
+  }
 
-    createTreeNode(node, parent, options, type, depth) {
-        const label = super.createTreeNode(node, parent, options, type, depth);
-        const icon = label.previousSibling;
-        const row = label.parentNode;
-        // Center arrow icon
-        if (icon) {
-            icon.style.backgroundPositionX = '5px';
-            icon.style.backgroundPositionY = '5px';
-        }
-        // Offset rows depending on their tree depth
-        row.style.padding = `5px`;
-        row.style.paddingLeft = `${5 + (type === 'leaf' ? 20 : 0) + depth * 20}px`;
-        return label;
+  onTreeNodeRightClick(tree, node, event) {
+    console.log("right-click", tree, node, event);
+  }
+
+  createTreeNode(node, parent, options, type, depth) {
+    const label = super.createTreeNode(node, parent, options, type, depth);
+    const icon = label.previousSibling;
+    const row = label.parentNode;
+    // Center arrow icon
+    if (icon) {
+      icon.style.backgroundPositionX = "5px";
+      icon.style.backgroundPositionY = "5px";
     }
+    // Offset rows depending on their tree depth
+    row.style.padding = `5px`;
+    row.style.paddingLeft = `${5 + (type === "leaf" ? 20 : 0) + depth * 20}px`;
+    return label;
+  }
 }
 
 export class CustomTreeViewPanel extends Autodesk.Viewing.UI.DockingPanel {
-    constructor(viewer, id, title) {
-        super(viewer.container, id, title);
-        this.container.classList.add('property-panel'); // Re-use some handy defaults
-        this.container.dockRight = true;
-        this.createScrollContainer({ left: false, heightAdjustment: 70, marginTop: 0 });
-        this.delegate = new CustomTreeDelegate();
-        this.tree = new Autodesk.Viewing.UI.Tree(this.delegate, MY_FOLDER_DATA, this.scrollContainer);
-    }
+  constructor(viewer, id, title) {
+    super(viewer.container, id, title);
+    this.container.classList.add("property-panel"); // Re-use some handy defaults
+    this.container.dockRight = true;
+    this.createScrollContainer({
+      left: false,
+      heightAdjustment: 70,
+      marginTop: 0,
+    });
+    this.delegate = new CustomTreeDelegate();
+    this.tree = new Autodesk.Viewing.UI.Tree(
+      this.delegate,
+      MY_FOLDER_DATA,
+      this.scrollContainer
+    );
+  }
 }
--- wwwroot/main.js
+++ wwwroot/main.js
@@ -1,31 +1,38 @@
-import { initViewer, loadModel } from './viewer.js';
+import { initViewer, loadModel } from "./viewer.js";
 
-initViewer(document.getElementById('preview')).then(viewer => {
-    const urn = window.location.hash?.substring(1);
-    setupModelSelection(viewer, urn);
+initViewer(document.getElementById("preview")).then((viewer) => {
+  const urn = window.location.hash?.substring(1);
+  setupModelSelection(viewer, urn);
 });
 
 async function setupModelSelection(viewer, selectedUrn) {
-    const dropdown = document.getElementById('models');
-    dropdown.innerHTML = '';
-    try {
-        const resp = await fetch('/api/models');
-        if (!resp.ok) {
-            throw new Error(await resp.text());
-        }
-        const models = await resp.json();
-        dropdown.innerHTML = models.map(model => `<option value=${model.urn} ${model.urn === selectedUrn ? 'selected' : ''}>${model.name}</option>`).join('\n');
-        dropdown.onchange = () => onModelSelected(viewer, dropdown.value);
-        if (dropdown.value) {
-            onModelSelected(viewer, dropdown.value);
-        }
-    } catch (err) {
-        alert('Could not list models. See the console for more details.');
-        console.error(err);
+  const dropdown = document.getElementById("models");
+  dropdown.innerHTML = "";
+  try {
+    const resp = await fetch("/api/models");
+    if (!resp.ok) {
+      throw new Error(await resp.text());
     }
+    const models = await resp.json();
+    dropdown.innerHTML = models
+      .map(
+        (model) =>
+          `<option value=${model.urn} ${
+            model.urn === selectedUrn ? "selected" : ""
+          }>${model.name}</option>`
+      )
+      .join("\n");
+    dropdown.onchange = () => onModelSelected(viewer, dropdown.value);
+    if (dropdown.value) {
+      onModelSelected(viewer, dropdown.value);
+    }
+  } catch (err) {
+    alert("Could not list models. See the console for more details.");
+    console.error(err);
+  }
 }
 
 async function onModelSelected(viewer, urn) {
-    window.location.hash = urn;
-    loadModel(viewer, urn);
+  window.location.hash = urn;
+  loadModel(viewer, urn);
 }
